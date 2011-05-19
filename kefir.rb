fae68b99718a5a0676b5a71d9a491d00c6bfa14a
futaro <machidada@gmail.com>
2011-05-19 18:49:22 +0900

    first commit

---
+++ kefir.rb
@@ -0,0 +1,48 @@
+#!/usr/bin/env ruby
+# -*- encoding: utf-8 -*-
+
+$LOAD_PATH << 'lib'
+
+IRC_ENCODING='sjis'
+
+require 'rubygems'
+require 'net/irc'
+require 'pp'
+require 'socket'
+require 'kconv'
+
+class IRCBot < Net::IRC::Client
+	
+	def initialize(*args)
+		super
+	end
+	
+	def on_rpl_welcome(m)
+		#post JOIN, "#kefir".encode(IRC_ENCODING).force_encoding('external')
+	end
+	
+	def on_invite(m)
+		post JOIN, m[1].encode(IRC_ENCODING).force_encoding('external')
+	end
+	
+	def on_privmsg(m)
+		
+		#msg = m[1].encode('external')
+		msg = m[1].toutf8
+		
+		if msg =~ /(?:か|ね|の)[?？]/u then
+			send_msg = m.prefix.nick + ': いいえ、ケフィアです。'
+			post PRIVMSG, m[0], send_msg.tosjis
+		end
+	end
+end
+
+IRCBot.new("irc.huie.hokudai.ac.jp", 6667, {
+	:nick => "kefir_bot", 
+	:user => "kefir",
+	:real => "kefir"
+}).start;
+
+
+
+
